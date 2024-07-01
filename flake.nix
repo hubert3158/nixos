d1e376cb98e2f0eaccddf01acca48137ea5e31a3
hubert <hubert3158@gmail.com>
2024-06-30 20:07:08 -0400

    Created home and work module
    wip
    
    wip
    
    wip
    
    wip

--- flake.nix
+++ flake.nix
@@ -8,16 +8,41 @@
 
   outputs = { self, nixpkgs, home-manager }: {
     nixosConfigurations = {
-      nixos = nixpkgs.lib.nixosSystem {
+      work = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
+          ./work.nix
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.backupFileExtension = "backup";
-            home-manager.users.hubert = import ./home.nix;
+            home-manager.users.hubert = let
+              pkgs = import nixpkgs { system = "x86_64-linux"; };
+              hmConfig = import ./home-manager-work.nix { inherit pkgs; };
+              packages = import ./home-manager-packages.nix { inherit pkgs; };
+            in
+              hmConfig // packages;
+          }
+        ];
+      };
+      home = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          ./home.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "backup";
+            home-manager.users.hubert = let
+              pkgs = import nixpkgs { system = "x86_64-linux"; };
+              hmConfig = import ./home-manager-home.nix { inherit pkgs; };
+              packages = import ./home-manager-packages.nix { inherit pkgs; };
+            in
+              hmConfig // packages;
           }
         ];
       };
---
+++ home-manager-config-files/neovim.nix
@@ -0,0 +1,12 @@
+{pkgs,...}:
+{
+ programs.neovim = {
+ enable = true;
+ plugins = with pkgs.vimPlugins; [
+   yankring
+   vim-nix
+   { plugin = vim-startify;
+     config = "let g:startify_change_to_vcs_root = 0";
+   }
+ ];};
+}
