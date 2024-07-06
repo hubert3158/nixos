d362985436ced404795581350f27a8b4f7e0aeb8
hubert <hubert3158@gmail.com>
2024-07-06 14:58:23 -0400

    added my previous nvim config, but idk, i might remove it again bruh

---
+++ home-manager-config-files/neovim/lua/core/plugin_config/notify.lua
@@ -0,0 +1,33 @@
+require("notify")("hey there")
+require("notify")("check out LudoPinelli/comment-box.nvim  <>cbccbox")
+require("notify")("install lazy git")
+
+
+local LazyVim = require("lazy")
+require("notify").setup({
+  keys = {
+    {
+      "<leader>un",
+      function()
+        require("notify").dismiss({ silent = true, pending = true })
+      end,
+      desc = "Dismiss All Notifications",
+    },
+  },
+  opts = {
+    stages = "static",
+    timeout = 3000,
+    max_height = function()
+      return math.floor(vim.o.lines * 0.75)
+    end,
+    max_width = function()
+      return math.floor(vim.o.columns * 0.75)
+    end,
+    on_open = function(win)
+      vim.api.nvim_win_set_config(win, { zindex = 100 })
+    end,
+  },
+  init = function()
+    -- when noice is not enabled, install notify on VeryLazy
+  end,
+})
