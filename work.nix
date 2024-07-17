32af241e5594b10720363bcfe0d66ad606beee9c
subash <subash@smartmca.com>
2024-07-17 09:36:08 -0400

    wip

--- work.nix
+++ work.nix
@@ -25,7 +25,7 @@
  # services.xserver.desktopManager.xfce.enable = true;
   services.xserver.displayManager.lightdm.enable = true;
 
-  services.xserver.desktopManager.plasma6.enable = true;
+  services.desktopManager.plasma6.enable = true;
     services.displayManager.sddm.wayland.enable = true;
 
 
