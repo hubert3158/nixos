c8179dee82d2627d23f19fbef51e1e42ad9b75e4
hubert <hubert3158@gmail.com>
2024-07-25 21:13:24 -0400

    wip

---
+++ scripts/tmux.sh
@@ -0,0 +1,46 @@
+
+#
+#-------------------- Local ----------------------
+
+
+# Start a new tmux session with the first window named Misc
+ tmux new-session -d -s local -n Misc 
+
+# Create new windows in the session
+tmux new-window -t local:1 -n Nvim -c ~/.config/nvim 'nvim init.lua'
+tmux new-window -t local:2 -n Influx-MCA-Web -c ~/dev/web/influx-mca-web 'nvim pom.xml'
+
+
+
+
+#-------------------- Local ----------------------
+
+#-------------------- Staging ----------------------
+
+
+tmux new-session -d -s stg -n Staging 'ssh stg.v2'
+tmux split-window -h -t stg:0
+
+# Send keys to the second pane to execute the desired commands
+tmux send-keys -t stg:0.1 'ssh stg.v2' C-m
+# tmux send-keys -t stg:0.1 'cd workspace/app/web/influx-mca-web/launch' C-m
+# tmux send-keys -t stg:0.1 'tail -f nohup.out' C-m
+
+#-------------------- Staging ----------------------
+
+
+#-------------------- Production ----------------------
+
+# tmux new-session -d -s prod -n Production 'ssh prod.v2'
+# tmux split-window -h -t prod:0
+
+# # Send keys to the second pane to execute the desired commands
+# tmux send-keys -t prod:0.1 'ssh prod.v2' C-m
+# tmux send-keys -t prod:0.1 'cd workspace/app/web/influx-mca-web/launch' C-m
+# tmux send-keys -t prod:0.1 'tail -f nohup.out' C-m
+
+#-------------------- Production ----------------------
+
+# Attach to the tmux session
+tmux attach -t local
+
