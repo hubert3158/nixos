de4145baf975e5a604fc599358aaa0e4bf36a965
hubert <hubert3158@gmail.com>
2024-07-31 20:59:10 -0400

    wip

---
+++ home-manager-config-files/xdg.nix
@@ -0,0 +1,41 @@
+{
+  xdg = {
+    enable = true;
+    desktopEntries = {
+      evolution = {
+        name = "Evolution";
+        genericName = "Application";
+        exec = "evolution";
+        terminal = false;
+        categories = [ "Email" "Application" "Network" ];
+        mimeType = [ "text/html" "text/xml" ];
+        comment = "It is a email client bro";
+      };
+      swayimg = {
+        name = "swayimg";
+        genericName = "Image Viewer";
+        exec = "swayimg %f";
+        terminal = false;
+        categories = [ "Graphics" "Viewer" ];
+        mimeType = [ "image/jpeg" "image/png" "image/gif" "image/bmp" "image/webp" "image/tiff" ];
+        comment = "It opens images bro";
+      };
+    };
+    mime = { enable = true; };
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "image/jpeg" = [ "swayimg.desktop" ];
+        "image/png" = [ "swayimg.desktop" ];
+        "image/gif" = [ "swayimg.desktop" ];
+        "image/bmp" = [ "swayimg.desktop" ];
+        "image/webp" = [ "swayimg.desktop" ];
+        "image/tiff" = [ "swayimg.desktop" ];
+        "application/pdf" = [ ".desktop" ];
+        "x-scheme-handler/msteams"=["teams-for-linux.desktop"];
+
+      };
+    };
+  };
+}
+
