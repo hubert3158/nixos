50a9f66d1743f90cdad10865eaf974a91ae0af54
hubert <hubert3158@gmail.com>
2024-07-31 21:35:00 -0400

    wip

---
+++ home-manager-config-files/nvim/plugin/surround.lua
@@ -0,0 +1,16 @@
+require"surround".setup {
+  context_offset = 100,
+  load_autogroups = false,
+  mappings_style = "sandwich",
+  map_insert_mode = true,
+  quotes = {"'", '"'},
+  brackets = {"(", '{', '['},
+  space_on_closing_char = false,
+  pairs = {
+    nestable = { b = { "(", ")" }, B = { "[", "]" }, c = { "{", "}" }, a = { "<", ">" } },
+    linear = { q = { "'", "'" }, t = { "`", "`" }, d = { '"', '"' } }
+  },
+  prefix = "s"
+}
+
+-- b = brackets, B = Big brackets, c = Curly Braces, a = Angle brackets, q = quote, t = tilde, d = double quote
