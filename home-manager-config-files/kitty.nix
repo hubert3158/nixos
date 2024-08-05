7ba83c0dad9f61cafd881620996b80d83dcfbb62
hubert <hubert3158@gmail.com>
2024-08-05 19:16:46 -0400

    wip

--- home-manager-config-files/kitty.nix
+++ home-manager-config-files/kitty.nix
@@ -6,5 +6,17 @@
       size = 14;
       package = pkgs.nerdfonts;
     };
+    settings = {
+      scrollback_lines = 10000;
+      enable_audio_bell = false;
+      update_check_interval = 0;
+    };
+    keybindings =  {
+        "ctrl+c" = "copy_or_interrupt";
+    };
+    shellIntegration = {
+      mode = "default"; 
+      enableZshIntegration =true;
+    };
   };
 }
