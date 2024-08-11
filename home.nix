8e955ba1f5c5cff0b8244c2a13d8edb39c82c491
hubert <hubert3158@gmail.com>
2024-08-10 20:48:43 -0400

    wip

--- home.nix
+++ home.nix
@@ -13,17 +13,19 @@
   };
 
   programs.hyprland.enable  = true;
-  # services.desktopManager.plasma6.enable = true;
    services.xserver = {
     enable = true;
     desktopManager.xfce.enable = true;
   };
-  # programs.hyprland.enable  = true;
+
+
   services.displayManager.sddm.enable = true;
 
   services.xserver = {
     videoDrivers = [ "nvidia" ];
   };
+
+
 
   hardware = {
     graphics = {
@@ -40,16 +42,7 @@
       open = true;
       modesetting.enable =  true;
       package = config.boot.kernelPackages.nvidiaPackages.stable; 
-    #Fixes a glitch
     nvidiaPersistenced = true;
-    #Required for amdgpu and nvidia gpu pairings
-    # prime = {
-    #   offload.enable = true;
-    #    #sync.enable = true;
-    #    amdgpuBusId = "PCI:5:0:0";
-    #    nvidiaBusId = "PCI:1:0:0";
-    #  };
-
   };
 };
 }
