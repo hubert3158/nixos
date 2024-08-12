62e2b7b802cb6647c0fa0754b23fd56262a4528b
subash <subash@smartmca.com>
2024-08-12 10:04:30 -0400

    wip

--- work.nix
+++ work.nix
@@ -1,4 +1,32 @@
-# Edit this configuration file to define what should be installed on
+# # Edit this configuration file to define what should be installed on
+# # your system.  Help is available in the configuration.nix(5) man page
+# # and in the NixOS manual (accessible by running ‘nixos-help’).
+#
+# { config, pkgs,lib, ... }:
+#
+# {
+#
+#   imports =
+#     [ 
+#       #./hardware-configuration-work.nix
+#     ];
+#
+#   # Configure keymap in X11
+#   services.xserver = {
+#     xkb.layout = "us";
+#     xkb.variant = "";
+#   };
+#
+#    hardware.bluetooth.enable = true;
+#   programs.hyprland.enable  = true;
+#   services.xserver.enable = true;
+#   hardware.graphics.enable = true;
+#   # services.desktopManager.plasma6.enable = true;   # somehow its needed for hyprland to work
+#   services.displayManager.sddm.enable = true;
+#   networking.firewall.allowedTCPPorts = [ 3000 8080];
+# services.intune.enable = true;
+#
+# }
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
@@ -6,44 +34,33 @@
 
 {
 
-  imports =
-    [ 
-      #./hardware-configuration-work.nix
-    ];
-
-  # Configure keymap in X11
+  ## Configure keymap in X11
   services.xserver = {
     xkb.layout = "us";
     xkb.variant = "";
   };
 
-   hardware.bluetooth.enable = true;
-
   programs.hyprland.enable  = true;
-  services.xserver.enable = true;
-  hardware.graphics.enable = true;
-  # services.desktopManager.plasma6.enable = true;   # somehow its needed for hyprland to work
+   services.xserver = {
+    enable = true;
+    desktopManager.xfce.enable = true;
+    windowManager.i3.enable = true;
+  };
+
+
   services.displayManager.sddm.enable = true;
 
-
-  networking.firewall.allowedTCPPorts = [ 3000 8080];
+  hardware = {
+    graphics = {
+      enable = true;
+      extraPackages = with pkgs; [
+        vaapiIntel
+        vaapiVdpau
+        libvdpau
+      ];
+    };
+    bluetooth.enable = true;
+};
+networking.firewall.allowedTCPPorts = [ 3000 8080];
 services.intune.enable = true;
-
-
-
-  # Enable the KDE Plasma Desktop Environment.
-#   services.displayManager.sddm.enable = true;
-# services.desktopManager.plasma6.enable = true;
-#services.xserver.desktopManager.gnome.enable = true;
- # services.xserver.desktopManager.xfce.enable = true;
-  # services.xserver.displayManager.lightdm.enable = true;
-  #
-  #   services.displayManager.sddm.wayland.enable = true;
-
-
-
-
-
-
-
 }
