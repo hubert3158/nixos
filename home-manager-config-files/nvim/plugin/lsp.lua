ccfcbb117d3672cd0446a06dbdc8e770377265d7
hubert <hubert3158@gmail.com>
2024-08-22 18:35:29 -0400

    wip

--- home-manager-config-files/nvim/plugin/lsp.lua
+++ home-manager-config-files/nvim/plugin/lsp.lua
@@ -37,7 +37,9 @@ local on_attach = function(client, bufnr)
     end, {})
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 require('neodev').setup()
