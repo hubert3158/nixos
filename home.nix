106811bdaef8045a7b3aa231de15b4717c560b43
Subash Acharya <hubert3158@gmail.com>
2025-01-05 20:59:25 -0500

    wip

--- home.nix
+++ home.nix
@@ -1,4 +1,32 @@
-# Edit this configuration file to define what should be installed on
+# # Edit this configuration file to define what should be installed on
+# # your system.  Help is available in the configuration.nix(5) man page
+# # and in the NixOS manual (accessible by running ‘nixos-help’).
+#
+# { config, pkgs,lib, ... }:
+#
+# {
+#
+#   imports =
+#     [ 
+#       #./hardware-configuration-work.nix
+#     ];
+#
+#   # Configure keymap in X11
+#   services.xserver = {
+#     xkb.layout = "us";
+#     xkb.variant = "";
+#   };
+#
+#    hardware.bluetooth.enable = true;
+#   programs.hyprland.enable  = true;
+#   services.xserver.enable = true;
+#   hardware.graphics.enable = true;
+#   # services.desktopManager.plasma6.enable = true;   # somehow its needed for hyprland to work
+#   services.displayManager.sddm.enable = true;
+#   networking.firewall.allowedTCPPorts = [ 3000 8080];
+# services.intune.enable = true;
+#
+# }
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
@@ -6,24 +34,25 @@
 
 {
 
+  ## Configure keymap in X11
+  services.xserver = {
+    xkb.layout = "us";
+    xkb.variant = "";
+  };
 
   programs.hyprland.enable  = true;
-
    services.xserver = {
     enable = true;
     desktopManager.xfce.enable = true;
     windowManager.i3.enable = true;
-    videoDrivers = [ "nvidia" ];
-    xkb.layout = "us";
-    xkb.variant = "";
   };
+
 
   services.displayManager.sddm.enable = true;
 
   hardware = {
     graphics = {
       enable = true;
-      enable32Bit = true;
       extraPackages = with pkgs; [
         vaapiIntel
         vaapiVdpau
@@ -31,12 +60,7 @@
       ];
     };
     bluetooth.enable = true;
-    nvidia = {
-      open = true;
-      modesetting.enable =  true;
-      package = config.boot.kernelPackages.nvidiaPackages.stable; 
-    nvidiaPersistenced = true;
-  };
 };
 networking.firewall.allowedTCPPorts = [ 3000 8080 8081 993 5678 5000];
+# services.intune.enable = true;
 }
