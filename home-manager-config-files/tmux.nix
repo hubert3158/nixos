bc83dd2c0615dd7ba057ceadd9879823ca807160
subash <subash@smartmca.com>
2025-01-28 14:39:02 -0500

    wip

--- home-manager-config-files/tmux.nix
+++ home-manager-config-files/tmux.nix
@@ -34,7 +34,7 @@ set -g @catppuccin_directory_text "#{pane_current_path}"
 	];
 	extraConfig = ''
 		set -g mouse on
-		set-option -sa terminal-features ',xterm-kitty:allow-passthrough'
+		set-option -g allow-passthrough all
 
 	'';
 };
