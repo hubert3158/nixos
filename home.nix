9ebb62238ef0d269ae818dc0b51659e6a7737ebf
Subash Acharya <hubert3158@gmail.com>
2025-02-14 02:31:25 -0500

    wip

--- home.nix
+++ home.nix
@@ -61,6 +61,6 @@
     };
     bluetooth.enable = true;
 };
-networking.firewall.allowedTCPPorts = [ 3000 8080 8081 993 5678 5000];
+networking.firewall.allowedTCPPorts = [ 3000 8080 8081 993 5678 5000 8083];
 # services.intune.enable = true;
 }
