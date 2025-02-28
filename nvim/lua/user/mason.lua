383a9624747492162203017f7e305c593f466cf6
Subash Acharya <hubert3158@gmail.com>
2025-02-28 02:45:37 -0500

    wip

---
+++ nvim/lua/user/mason.lua
@@ -0,0 +1,16 @@
+require("mason").setup(
+    {
+        ui = {
+            icons = {
+                package_installed = "✓",
+                package_pending = "➜",
+                package_uninstalled = "✗",
+            },
+        },
+        ensure_installed = {
+            "json-lsp"
+        },
+    }
+
+)
+
