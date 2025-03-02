152cccc0fc2d11dfa974a9b79357a72b1f7ba666
Subash Acharya <hubert3158@gmail.com>
2025-03-02 05:40:47 -0500

    wip

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -7,23 +7,30 @@
  tmux new-session -d -s local -n Misc 
 
 # Create new windows in the session
-tmux new-window -t local:1 -n Nvim -c ~/.config/nvim 'nvim init.lua'
-tmux new-window -t local:2 -n Influx-MCA-Web -c ~/dev/web/influx-mca-web 'nvim pom.xml'
-
-
-
+# tmux new-window -t local:1 -n Nvim -c ~/.config/nvim 'nvim init.lua'
+# tmux new-window -t local:2 -n Influx-MCA-Web -c ~/dev/web/influx-mca-web 'nvim pom.xml'
 
 #-------------------- Local ----------------------
+
+#-------------------- Development----------------------
+
+# Start a new tmux session with the first window named Misc
+ tmux new-session -d -s dev -n Development
+
+# Create new windows in the session
+
+#-------------------- Development ----------------------
+
 
 #-------------------- Staging ----------------------
 
 
-tmux new-session -d -s stg -n Staging 'ssh stg.v2'
-tmux split-window -h -t stg:0
-
-# Send keys to the second pane to execute the desired commands
-tmux send-keys -t stg:0.1 'ssh stg.v2' C-m
-# tmux send-keys -t stg:0.1 'cd workspace/app/web/influx-mca-web/launch' C-m
+# tmux new-session -d -s stg -n Staging 'ssh stg.v2'
+# tmux split-window -h -t stg:0
+#
+# # Send keys to the second pane to execute the desired commands
+# tmux send-keys -t stg:0.1 'ssh stg.v2' C-m
+# # tmux send-keys -t stg:0.1 'cd workspace/app/web/influx-mca-web/launch' C-m
 # tmux send-keys -t stg:0.1 'tail -f nohup.out' C-m
 
 #-------------------- Staging ----------------------
