e36c56cbf5e17e4fa2061b1853bf83f97fc9689b
Subash Acharya <hubert3158@gmail.com>
2025-03-24 23:23:21 -0400

    wip

--- nvim/plugin/dap.lua
+++ nvim/plugin/dap.lua
@@ -3,24 +3,35 @@ local dapui = require('dapui')
 
 dapui.setup()
 
-dap.adapters.java = {
-    type = 'server',
-    host = '127.0.0.1',
-    port = 5005,
-    options = {
-        timeout = 10000 -- Increase the timeout to 10 seconds
-    }
+-- Open DAP UI automatically on debugging start
+dap.listeners.after.event_initialized["dapui_config"] = function()
+    dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+    dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+    dapui.close()
+end
+
+dap.adapters.firefox = {
+  type = 'executable',
+  command = 'node',
+  args = {'/nix/store/fczhm3sb9mh44v4zgzlmlms6jaicxbrs-vscode-extension-firefox-devtools-vscode-firefox-debug-2.9.10/share/vscode/extensions/firefox-devtools.vscode-firefox-debug/dist/adapter.bundle.js'},
 }
 
-dap.configurations.java = {
-    {
-        type = 'java',
-        request = 'attach',
-        name = "Attach to Java process",
-        hostName = "127.0.0.1",
-        port = 5005,
-    },
+dap.configurations.typescript = {
+  {
+  name = 'Debug with Firefox',
+  type = 'firefox',
+  request = 'launch',
+  reAttach = true,
+  url = 'http://localhost:3000',
+  webRoot = '${workspaceFolder}',
+  firefoxExecutable = '/run/current-system/sw/bin/firefox'
+  }
 }
 
-dap.set_log_level('TRACE')
+dap.configurations.javascript = dap.configurations.typescript
+dap.configurations.typescriptreact = dap.configurations.javascript
 
