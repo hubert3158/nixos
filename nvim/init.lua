928c63962b7b0455ad3ac7846a811c1def895095
subash <subash@smartmca.com>
2025-03-25 10:00:24 -0400

    wip

--- nvim/init.lua
+++ nvim/init.lua
@@ -292,13 +292,13 @@ vim.api.nvim_set_keymap('n', "<leader>gg", ":LazyGit<CR>",
 
 
 -- DAP (Debug Adapter Protocol) UI toggling, continue, disconnect, stepping, and breakpoints
-vim.api.nvim_set_keymap('n', 'et', ":lua require('dapui').toggle()<CR>", { noremap = true, silent = true, desc = "Toggle DAP UI" })
-vim.api.nvim_set_keymap('n', 'ec', ":lua require('dap').continue()<CR>", { noremap = true, silent = true, desc = "DAP Continue" })
-vim.api.nvim_set_keymap('n', 'ed', ":lua require('dap').disconnect()<CR>", { noremap = true, silent = true, desc = "DAP Disconnect" })
-vim.api.nvim_set_keymap('n', 'es', ":lua require('dap').step_over()<CR>", { noremap = true, silent = true, desc = "DAP Step Over" })
-vim.api.nvim_set_keymap('n', 'ei', ":lua require('dap').step_into()<CR>", { noremap = true, silent = true, desc = "DAP Step Into" })
-vim.api.nvim_set_keymap('n', 'eo', ":lua require('dap').step_out()<CR>", { noremap = true, silent = true, desc = "DAP Step Out" })
+vim.api.nvim_set_keymap('n', '<F1>', ":lua require('dapui').toggle()<CR>", { noremap = true, silent = true, desc = "Toggle DAP UI" })
+vim.api.nvim_set_keymap('n', '<F4>', ":lua require('dap').step_over()<CR>", { noremap = true, silent = true, desc = "DAP Step Over" })
+vim.api.nvim_set_keymap('n', '<F5>', ":lua require('dap').continue()<CR>", { noremap = true, silent = true, desc = "DAP Continue" })
+vim.api.nvim_set_keymap('n', '<F3>', ":lua require('dap').step_into()<CR>", { noremap = true, silent = true, desc = "DAP Step Into" })
+vim.api.nvim_set_keymap('n', '<F2>', ":lua require('dap').step_out()<CR>", { noremap = true, silent = true, desc = "DAP Step Out" })
 
+vim.api.nvim_set_keymap('n', 'ed', ":lua require('dap').disconnect()<CR>", { noremap = true, silent = true, desc = "DAP Disconnect" })
 -- Breakpoints: toggle breakpoint, conditional breakpoint, and clear all breakpoints
 vim.api.nvim_set_keymap('n', 'eb', ":lua require('dap').toggle_breakpoint()<CR>", { noremap = true, silent = true, desc = "Toggle Breakpoint" })
 vim.api.nvim_set_keymap('n', 'eB', ":lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>", { noremap = true, silent = true, desc = "Set Conditional Breakpoint" })
