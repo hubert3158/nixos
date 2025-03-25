df31ddbc88265a28a044838f8627c704bf1488fc
Subash Acharya <hubert3158@gmail.com>
2025-03-24 20:14:12 -0400

    wip

--- configuration.nix
+++ configuration.nix
@@ -33,7 +33,13 @@
 
   # Enable networking
   networking.networkmanager.enable = true;
-  networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
+  networking.nameservers = [
+  "1.1.1.1"  # Cloudflare
+  "1.0.0.1"  # Cloudflare secondary
+  "8.8.8.8"  # Google
+  "8.8.4.4"  # Google secondary
+];
+
 
 
   # Set your time zone.
