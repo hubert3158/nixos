fe3d85a510a3ac533afba0400a33f4300c7dc6b3
subash <subash@smartmca.com>
2025-03-28 14:10:49 -0400

    wip

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -1,58 +1,126 @@
--- local parser_install_dir = vim.fn.expand('/home/hubert/temp')
+-- ~/.config/nvim/lua/plugins/treesitter.lua (or wherever you configure plugins)
 
-require('nvim-treesitter.configs').setup {
-	-- ensure_installed = { "c", "query", "lua", "java", "nix", "vimdoc", "luadoc", "vim", "markdown", "markdown_inline" },
-	-- This is not needed / causes problems
-
-	auto_install = false,
-
-	highlight = {
-		enable = true,
-		-- disable = { "c", "rust","vimdoc" },
-	},
-	indent = { enable = true },
-     textobjects = {
-    select = {
-      enable = true,
-
-      -- Automatically jump forward to textobj, similar to targets.vim
-      lookahead = true,
-
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ["af"] = "@function.outer",
-        ["if"] = "@function.inner",
-        ["ac"] = "@class.outer",
-        -- You can optionally set descriptions to the mappings (used in the desc parameter of
-        -- nvim_buf_set_keymap) which plugins like which-key display
-        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-        -- You can also use captures from other query groups like `locals.scm`
-        ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+return {
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate', -- Recommended to keep parsers updated
+  config = function()
+    require('nvim-treesitter.configs').setup {
+      -- A list of parser names, or "all"
+      -- *** Add the languages you commonly use here ***
+      ensure_installed = {
+        "bash",
+        "c",
+        "cpp",
+        "css",
+        "diff",
+        "go",
+        "html",
+        "java",
+        "javascript",
+        "json",
+        "lua",
+        "luadoc",
+        "luap", -- Lua patterns
+        "markdown",
+        "markdown_inline",
+        "nix",
+        "python",
+        "query", -- Treesitter's own query language
+        "regex",
+        "rust",
+        "toml",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "yaml",
       },
-      -- You can choose the select mode (default is charwise 'v')
-      --
-      -- Can also be a function which gets passed a table with the keys
-      -- * query_string: eg '@function.inner'
-      -- * method: eg 'v' or 'o'
-      -- and should return the mode ('v', 'V', or '<c-v>') or a table
-      -- mapping query_strings to modes.
-      selection_modes = {
-        ['@parameter.outer'] = 'v', -- charwise
-        ['@function.outer'] = 'V', -- linewise
-        ['@class.outer'] = '<c-v>', -- blockwise
+
+      -- Install parsers synchronously (only applied to `ensure_installed`)
+      sync_install = false,
+
+      -- Automatically install missing parsers when entering buffer
+      -- Recommendation: set to true for convenience
+      auto_install = true,
+
+      -- List of parsers to ignore installing (or "all")
+      -- ignore_install = { "javascript" },
+
+      highlight = {
+        -- `false` will disable the whole extension
+        enable = true,
+
+        -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+        -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is the name of the parser)
+        -- disable = { "c", "rust" },
+        -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+        -- disable = function(lang, buf)
+        --     local max_filesize = 100 * 1024 -- 100 KB
+        --     local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        --     if ok and stats and stats.size > max_filesize then
+        --         return true
+        --     end
+        -- end,
+
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
       },
-      -- If you set this to `true` (default is `false`) then any textobject is
-      -- extended to include preceding or succeeding whitespace. Succeeding
-      -- whitespace has priority in order to act similarly to eg the built-in
-      -- `ap`.
-      --
-      -- Can also be a function which gets passed a table with the keys
-      -- * query_string: eg '@function.inner'
-      -- * selection_mode: eg 'v'
-      -- and should return true or false
-      include_surrounding_whitespace = true,
-    },
-  },
+
+      indent = {
+        enable = true,
+        -- disable = { "yaml" }
+      },
+
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ["af"] = { query = "@function.outer", desc = "Select outer function" },
+            ["if"] = { query = "@function.inner", desc = "Select inner function" },
+            ["ac"] = { query = "@class.outer", desc = "Select outer class" },
+            ["ic"] = { query = "@class.inner", desc = "Select inner class" },
+            -- You can also use captures from other query groups like `locals.scm`
+            ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+            -- Simpler mappings (similar to default):
+            ["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
+            ["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
+            ["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
+            ["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
+            ["a:"] = { query = "@parameter.outer", desc = "Select outer part of parameter/argument" },
+            ["i:"] = { query = "@parameter.inner", desc = "Select inner part of parameter/argument" },
+            -- Add more keymaps based on available captures in parsers' textobjects.scm files
+          },
+          selection_modes = {
+            ['@parameter.outer'] = 'v', -- charwise
+            ['@function.outer'] = 'V', -- linewise
+            ['@class.outer'] = 'V', -- linewise (often more useful than blockwise for classes)
+            -- Add other specific modes if needed
+          },
+          -- If you set this to `true` methods like `am` and `im` will act like the Vim originals behavior
+          include_surrounding_whitespace = true,
+        },
+        -- You can also configure swap / move / etc.
+        -- move = { ... }
+        -- swap = { ... }
+      },
+
+      -- Other modules (disabled by default)
+      -- incremental_selection = { enable = true },
+      -- refactor = { ... }
+      -- navigation = { ... }
+      -- etc.
+    }
+
+    -- Folding configuration (outside the setup table)
+    -- Uses Treesitter to determine fold regions
+    vim.opt.foldmethod = 'expr'
+    vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+    vim.opt.foldlevel = 99      -- Default fold level (e.g., 0 means all folded, 99 means all open)
+    vim.opt.foldlevelstart = 99 -- Fold level for new buffers (start with folds open)
+    vim.opt.foldenable = true   -- Enable folding overall
+  end,
 }
-
-
