35979216694ee3662d486278785de6deab5e14f0
Subash Acharya <hubert3158@gmail.com>
2025-03-30 04:53:43 -0400

    wip

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -1,100 +1,99 @@
-local lspconfig = require 'lspconfig'
-
+local lspconfig = require("lspconfig")
 
 local on_attach = function(client, bufnr)
-    local bufmap = function(keys, func)
-        vim.keymap.set('n', keys, func, { buffer = bufnr })
-    end
+	local bufmap = function(keys, func)
+		vim.keymap.set("n", keys, func, { buffer = bufnr })
+	end
 
+	-- Enable auto-formatting on save
+	-- if client.server_capabilities.documentFormattingProvider then
+	--     vim.api.nvim_command([[augroup Format]])
+	--     vim.api.nvim_command([[autocmd! * <buffer>]])
+	--     vim.api.nvim_command([[autocmd BufWritePre <buffer> lua vim.lsp.buf.format({ async = true })]])
+	--     vim.api.nvim_command([[augroup END]])
+	-- end
+	--
+	--
+	--
 
-    -- Enable auto-formatting on save
-    -- if client.server_capabilities.documentFormattingProvider then
-    --     vim.api.nvim_command([[augroup Format]])
-    --     vim.api.nvim_command([[autocmd! * <buffer>]])
-    --     vim.api.nvim_command([[autocmd BufWritePre <buffer> lua vim.lsp.buf.format({ async = true })]])
-    --     vim.api.nvim_command([[augroup END]])
-    -- end
-    --
-    --
-    --
+	-- Additional key mappings for linting
+	-- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>l', '<cmd>EslintFixAll<CR>', { noremap = true, silent = true })
+	-- bufmap('<leader>l', "<cmd>EslintFixAll<CR>")
 
-    -- Additional key mappings for linting
-    -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>l', '<cmd>EslintFixAll<CR>', { noremap = true, silent = true })
-    -- bufmap('<leader>l', "<cmd>EslintFixAll<CR>")
+	-- bufmap('<leader>r', vim.lsp.buf.rename)
+	-- bufmap('<leader>a', vim.lsp.buf.code_action)
 
-    -- bufmap('<leader>r', vim.lsp.buf.rename)
-    -- bufmap('<leader>a', vim.lsp.buf.code_action)
+	bufmap("gd", vim.lsp.buf.definition)
+	bufmap("gD", vim.lsp.buf.declaration)
+	bufmap("gI", vim.lsp.buf.implementation)
+	bufmap("gl", vim.lsp.buf.implementation)
+	bufmap("<leader>D", vim.lsp.buf.type_definition)
 
-    bufmap('gd', vim.lsp.buf.definition)
-    bufmap('gD', vim.lsp.buf.declaration)
-    bufmap('gI', vim.lsp.buf.implementation)
-    bufmap('gl', vim.lsp.buf.implementation)
-    bufmap('<leader>D', vim.lsp.buf.type_definition)
+	bufmap("gl", vim.diagnostic.open_float) -- Show diagnostics in a floating window
 
-    bufmap('gl', vim.diagnostic.open_float)                     -- Show diagnostics in a floating window
+	bufmap("[d", function()
+		vim.diagnostic.jump({ count = -1, float = true })
+	end) -- Go to the previous diagnostic
 
-    bufmap('[d', function()
-  vim.diagnostic.jump({ count = -1, float = true })
-end) -- Go to the previous diagnostic
+	bufmap("]d", function()
+		vim.diagnostic.jump({ count = 1, float = true })
+	end) -- Go to the next diagnostic
 
-bufmap(']d', function()
-  vim.diagnostic.jump({ count = 1, float = true })
-end) -- Go to the next diagnostic
+	bufmap("[e", function()
+		vim.diagnostic.jump({ count = -1, severity = vim.diagnostic.severity.ERROR, float = true })
+	end) -- Go to the previous error diagnostic
 
-bufmap('[e', function()
-  vim.diagnostic.jump({ count = -1, severity = vim.diagnostic.severity.ERROR, float = true })
-end) -- Go to the previous error diagnostic
+	bufmap("]e", function()
+		vim.diagnostic.jump({ count = 1, severity = vim.diagnostic.severity.ERROR, float = true })
+	end) -- Go to the next error diagnostic
 
-bufmap(']e', function()
-  vim.diagnostic.jump({ count = 1, severity = vim.diagnostic.severity.ERROR, float = true })
-end) -- Go to the next error diagnostic
+	bufmap("<leader>lq", vim.diagnostic.setloclist) -- Show all diagnostics in the location list
+	bufmap("<leader>lQ", vim.diagnostic.setqflist) -- Show all diagnostics in the quickfix list
+	bufmap("<leader>ld", function() -- Toggle virtual text on/off
+		vim.diagnostic.config({ virtual_text = not vim.diagnostic.config().virtual_text })
+	end)
 
-    bufmap('<leader>lq', vim.diagnostic.setloclist)             -- Show all diagnostics in the location list
-    bufmap('<leader>lQ', vim.diagnostic.setqflist)              -- Show all diagnostics in the quickfix list
-    bufmap('<leader>ld', function()                             -- Toggle virtual text on/off
-        vim.diagnostic.config({ virtual_text = not vim.diagnostic.config().virtual_text })
-    end)
+	-- bufmap('gr', require('telescope.builtin').lsp_references)
+	bufmap("gs", require("telescope.builtin").lsp_document_symbols)
 
-    -- bufmap('gr', require('telescope.builtin').lsp_references)
-    bufmap('gs', require('telescope.builtin').lsp_document_symbols)
+	-- the following can be achieve by <leader>fs then chose the builtin you want
+	-- bufmap('<leader>s', require('telescope.builtin').lsp_document_symbols)
+	-- bufmap('<leader>S', require('telescope.builtin').lsp_dynamic_workspace_symbols)
 
-    -- the following can be achieve by <leader>fs then chose the builtin you want
-    -- bufmap('<leader>s', require('telescope.builtin').lsp_document_symbols)
-    -- bufmap('<leader>S', require('telescope.builtin').lsp_dynamic_workspace_symbols)
+	bufmap("K", vim.lsp.buf.hover)
 
-    bufmap('K', vim.lsp.buf.hover)
-
-    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-        vim.lsp.buf.format()
-    end, {})
+	vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+		vim.lsp.buf.format()
+	end, {})
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-require('neodev').setup()
-require('lspconfig').lua_ls.setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-    root_dir = function()
-        return vim.loop.cwd()
-    end,
-    cmd = { "lua-language-server" },
-    settings = {
-        Lua = {
-            workspace = { checkThirdParty = false },
-            telemetry = { enable = false },
-        },
-    }
-}
+require("neodev").setup()
+require("lspconfig").lua_ls.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	root_dir = function()
+		return vim.loop.cwd()
+	end,
+	cmd = { "lua-language-server" },
+	settings = {
+		Lua = {
+			workspace = { checkThirdParty = false },
+			telemetry = { enable = false },
+		},
+	},
+})
 
-require('lspconfig').nil_ls.setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-}
+require("lspconfig").nil_ls.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
+
 require("lspconfig").html.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
+	on_attach = on_attach,
+	capabilities = capabilities,
 })
 -- require("lspconfig").bashls.setup({
 -- 	on_attach = on_attach,
@@ -102,45 +101,39 @@ require("lspconfig").html.setup({
 -- })
 
 require("lspconfig").zls.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
+	on_attach = on_attach,
+	capabilities = capabilities,
 })
-
 
 require("lspconfig").eslint.setup({
-    on_attach = function(client, bufnr)
-        -- Ensure keybindings and other LSP-specific features work
-        if type(on_attach) == "function" then
-            on_attach(client, bufnr)
-        end
+	on_attach = function(client, bufnr)
+		-- Ensure keybindings and other LSP-specific features work
+		if type(on_attach) == "function" then
+			on_attach(client, bufnr)
+		end
 
-        -- Auto-fix ESLint issues before saving the file
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            callback = function()
-                vim.lsp.buf.format({ async = false }) -- Use LSP's built-in formatting
-            end,
-        })
-    end,
-    capabilities = capabilities or vim.lsp.protocol.make_client_capabilities(),
-    root_dir = require("lspconfig").util.find_package_json_ancestor,
-    -- cmd = { "vscode-eslint-language-server", "--stdio" },
+		-- Auto-fix ESLint issues before saving the file
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			buffer = bufnr,
+			callback = function()
+				vim.lsp.buf.format({ async = false }) -- Use LSP's built-in formatting
+			end,
+		})
+	end,
+	capabilities = capabilities or vim.lsp.protocol.make_client_capabilities(),
+	root_dir = require("lspconfig").util.find_package_json_ancestor,
+	-- cmd = { "vscode-eslint-language-server", "--stdio" },
 })
-
-
-
-
 
 require("lspconfig").sourcekit.setup({ -- c++
-    on_attach = on_attach,
-    capabilities = capabilities,
+	on_attach = on_attach,
+	capabilities = capabilities,
 })
 
-
-require 'lspconfig'.clangd.setup { -- c
-    on_attach = on_attach,
-    capabilities = capabilities,
-}
+require("lspconfig").clangd.setup({ -- c
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
 
 -- lspconfig.ccls.setup {
 --     root_dir = function(fname)
@@ -157,82 +150,74 @@ require 'lspconfig'.clangd.setup { -- c
 --     }
 -- }
 
-
-
 require("lspconfig").ts_ls.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    cmd = { "typescript-language-server", "--stdio" },
+	on_attach = on_attach,
+	capabilities = capabilities,
+	cmd = { "typescript-language-server", "--stdio" },
 })
-
-
-
-
 
 -- require("lspconfig").jdtls.setup({
 --     on_attach = on_attach,
 --     capabilities = capabilities,
 -- })
 
+require("lspconfig").pyright.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
 
-require 'lspconfig'.pyright.setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-}
-
-require 'lspconfig'.cssls.setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-}
-
+require("lspconfig").cssls.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
 
 -- local lombok_path = vim.fn.stdpath("data") .. "/lombok.jar"  -- Adjust the path if necessary
 local lombok_path = "/home/hubert/nixos/dotfiles/lombok.jar"
 require("lspconfig").jdtls.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    cmd = { "/etc/profiles/per-user/hubert/bin/jdtls" }, -- Update the path to your jdtls executable
+	on_attach = on_attach,
+	capabilities = capabilities,
+	cmd = { "/etc/profiles/per-user/hubert/bin/jdtls" }, -- Update the path to your jdtls executable
 
-    root_dir = function(fname)
-        return require('lspconfig.util').root_pattern('pom.xml', 'build.gradle', '.git')(fname) or
-            vim.loop.os_homedir()
-    end,
-    settings = {
-        java = {
-            signatureHelp = { enabled = true },
-            contentProvider = { preferred = 'fernflower' },
-            completion = {
-                favoriteStaticMembers = {
-                    "org.hamcrest.MatcherAssert.assertThat",
-                    "org.hamcrest.Matchers.*",
-                    "org.hamcrest.CoreMatchers.*",
-                    "org.junit.jupiter.api.Assertions.*",
-                    "java.util.Objects.requireNonNull",
-                    "java.util.Objects.requireNonNullElse",
-                }
-            },
-            project = {
-                referencedLibraries = {
-                    lombok_path,
-                }
-            },
-        },
-    },
-    init_options = {
-        bundles = {
-            lombok_path,
-            vim.fn.glob(
-                '/nix/store/id0zrxghssr6mkzxaaphs9yy1sjn7f57-vscode-extension-vscjava-vscode-java-debug-0.55.2023121302/share/vscode/extensions/vscjava.vscode-java-debug/server/com.microsoft.java.debug.plugin-0.50.0.jar',
-                true),
-        }
-    },
+	root_dir = function(fname)
+		return require("lspconfig.util").root_pattern("pom.xml", "build.gradle", ".git")(fname) or vim.loop.os_homedir()
+	end,
+	settings = {
+		java = {
+			signatureHelp = { enabled = true },
+			contentProvider = { preferred = "fernflower" },
+			completion = {
+				favoriteStaticMembers = {
+					"org.hamcrest.MatcherAssert.assertThat",
+					"org.hamcrest.Matchers.*",
+					"org.hamcrest.CoreMatchers.*",
+					"org.junit.jupiter.api.Assertions.*",
+					"java.util.Objects.requireNonNull",
+					"java.util.Objects.requireNonNullElse",
+				},
+			},
+			project = {
+				referencedLibraries = {
+					lombok_path,
+				},
+			},
+		},
+	},
+	init_options = {
+		bundles = {
+			lombok_path,
+			vim.fn.glob(
+				"/nix/store/id0zrxghssr6mkzxaaphs9yy1sjn7f57-vscode-extension-vscjava-vscode-java-debug-0.55.2023121302/share/vscode/extensions/vscjava.vscode-java-debug/server/com.microsoft.java.debug.plugin-0.50.0.jar",
+				true
+			),
+		},
+	},
 })
 
 -- require'lspconfig'.jsonls.setup { // this has been replaced by conform
 --     on_attach = on_attach,
 --     capabilities = capabilities,
 -- }
-require'lspconfig'.nginx_language_server.setup{
-    on_attach = on_attach,
-    capabilities = capabilities,
-}
+require("lspconfig").nginx_language_server.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
