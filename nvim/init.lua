5df6e3960e7534ea1bdbee21089441629c7a2321
Subash Acharya <hubert3158@gmail.com>
2025-04-01 02:28:00 -0400

    cdd- Added refactoring-nvim plugin to neovim-overla
    - Added refactoring-nvim plugin to neovim-overlay.nix with basic setup
    configuration.
    - Configured key mappings in init.lua for various refactoring operations
    such as Extract Function, Extract Variable, Inline Function, and more.

--- nvim/init.lua
+++ nvim/init.lua
@@ -524,6 +524,35 @@ vim.api.nvim_set_keymap(
 	{ noremap = true, silent = true, desc = "[M]isscellineous [T]wilight" }
 )
 
+-- refactor
+vim.keymap.set({ "n", "x" }, "<leader>re", function()
+	return require("refactoring").refactor("Extract Function")
+end, { expr = true, desc = "Extract Function" })
+
+vim.keymap.set({ "n", "x" }, "<leader>rf", function()
+	return require("refactoring").refactor("Extract Function To File")
+end, { expr = true, desc = "Extract Function To File" })
+
+vim.keymap.set({ "n", "x" }, "<leader>rv", function()
+	return require("refactoring").refactor("Extract Variable")
+end, { expr = true, desc = "Extract Variable" })
+
+vim.keymap.set({ "n", "x" }, "<leader>rI", function()
+	return require("refactoring").refactor("Inline Function")
+end, { expr = true, desc = "Inline Function" })
+
+vim.keymap.set({ "n", "x" }, "<leader>ri", function()
+	return require("refactoring").refactor("Inline Variable")
+end, { expr = true, desc = "Inline Variable" })
+
+vim.keymap.set({ "n", "x" }, "<leader>rbb", function()
+	return require("refactoring").refactor("Extract Block")
+end, { expr = true, desc = "Extract Block" })
+
+vim.keymap.set({ "n", "x" }, "<leader>rbf", function()
+	return require("refactoring").refactor("Extract Block To File")
+end, { expr = true, desc = "Extract Block To File" })
+
 -- Configure Neovim diagnostic messages
 local function prefix_diagnostic(prefix, diagnostic)
 	return string.format(prefix .. " %s", diagnostic.message)
