d024f2dcd227c772f810d2886539015a8e8bd86f
Subash Acharya <hubert3158@gmail.com>
2025-04-10 01:44:39 -0400

    added anthropic adapter

--- nvim/lua/user/codeCompanion.lua
+++ nvim/lua/user/codeCompanion.lua
@@ -1,5 +1,14 @@
 require("codecompanion").setup({
-  opts = {
-    log_level = "DEBUG", -- or "TRACE"
-  }
+	opts = {
+		log_level = "DEBUG", -- or "TRACE"
+	},
+	adapters = {
+		anthropic = function()
+			return require("codecompanion.adapters").extend("anthropic", {
+				env = {
+					api_key = "cmd: gpg --batch --quiet --decrypt ~/.password-store/keys/api/anthropic.gpg",
+				},
+			})
+		end,
+	},
 })
