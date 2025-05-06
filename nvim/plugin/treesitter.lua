b37bbf51a685af1f9b12d65e64451886cd696736
Subash Acharya <hubert3158@gmail.com>
2025-05-06 01:21:12 -0400

    huge update on textobjects

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -1,114 +1,116 @@
+-- Or wherever you configure your plugins
+
+-- Define the directory for storing Treesitter parsers (still needed by nvim-treesitter itself)
 local parser_install_dir = vim.fn.stdpath("data") .. "/treesitter-parsers"
 
+-- Configure nvim-treesitter
 require("nvim-treesitter.configs").setup({
 	modules = {},
-	ignore_install = {},
+	ensure_installed = {},
+	-- Specify the installation directory for parsers
 	parser_install_dir = parser_install_dir,
-	-- A list of parser names, or "all"
-	ensure_installed = {
-		-- "bash",
-		-- "c",
-		-- "cpp",
-		-- "css",
-		-- "diff",
-		-- "go",
-		-- "html",
-		-- "java",
-		-- "javascript",
-		-- "json",
-		-- "lua",
-		-- "luadoc",
-		-- "luap", -- Lua patterns
-		-- "markdown",
-		-- "markdown_inline",
-		-- "nix",
-		-- "python",
-		-- "query", -- Treesitter's own query language
-		-- "regex",
-		-- "rust",
-		-- "toml",
-		-- "typescript",
-		-- "vim",
-		-- "vimdoc",
-		-- "yaml",
-	},
 
-	-- Install parsers synchronously (only applied to `ensure_installed`)
+	-- Install parsers synchronously (blocking Neovim)
 	sync_install = false,
 
-	-- Automatically install missing parsers when entering buffer
-	-- Recommendation: set to true for convenience
-	auto_install = true,
+	-- Automatically install missing parsers when opening a buffer
+	-- Set to false if NixOS handles *all* parser installations reliably
+	auto_install = false, -- Changed to false based on NixOS handling
 
-	-- List of parsers to ignore installing (or "all")
-	-- ignore_install = { "javascript" },
+	-- List of parsers to ignore installing (can be "all")
+	ignore_install = {}, -- Keep empty or list specific parsers if needed
 
+	-- Configure highlighting
 	highlight = {
-		-- `false` will disable the whole extension
 		enable = true,
-
-		-- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-		-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is the name of the parser)
+		-- Disable highlighting for specific languages if needed
 		-- disable = { "c", "rust" },
-		-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-		-- disable = function(lang, buf)
-		--     local max_filesize = 100 * 1024 -- 100 KB
-		--     local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-		--     if ok and stats and stats.size > max_filesize then
-		--         return true
-		--     end
-		-- end,
-
-		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-		-- Using this option may slow down your editor, and you may see some duplicate highlights.
-		-- Instead of true it can also be a list of languages
 		additional_vim_regex_highlighting = false,
 	},
 
+	-- Configure indentation based on Treesitter
 	indent = {
 		enable = true,
-		-- disable = { "yaml" }
+		-- disable = { "yaml" },
 	},
 
+	-- Configure Treesitter textobjects
 	textobjects = {
+		-- Textobject selection module
 		select = {
 			enable = true,
-			lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+			lookahead = true,
 			keymaps = {
-				-- You can use the capture groups defined in textobjects.scm
 				["af"] = { query = "@function.outer", desc = "Select outer function" },
 				["if"] = { query = "@function.inner", desc = "Select inner function" },
 				["ac"] = { query = "@class.outer", desc = "Select outer class" },
 				["ic"] = { query = "@class.inner", desc = "Select inner class" },
-				-- You can also use captures from other query groups like `locals.scm`
 				["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-				-- Simpler mappings (similar to default):
 				["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
 				["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
 				["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
 				["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
 				["a:"] = { query = "@parameter.outer", desc = "Select outer part of parameter/argument" },
 				["i:"] = { query = "@parameter.inner", desc = "Select inner part of parameter/argument" },
-				-- Add more keymaps based on available captures in parsers' textobjects.scm files
 			},
 			selection_modes = {
-				["@parameter.outer"] = "v", -- charwise
-				["@function.outer"] = "V", -- linewise
-				["@class.outer"] = "V", -- linewise (often more useful than blockwise for classes)
-				-- Add other specific modes if needed
+				["@parameter.outer"] = "v",
+				["@function.outer"] = "V",
+				["@class.outer"] = "V",
+				["@assignment.outer"] = "V",
+				["@loop.outer"] = "V",
+				["@conditional.outer"] = "V",
 			},
-			-- If you set this to `true` methods like `am` and `im` will act like the Vim originals behavior
 			include_surrounding_whitespace = true,
 		},
-		-- You can also configure swap / move / etc.
-		-- move = { ... }
-		-- swap = { ... }
-	},
 
-	-- Other modules (disabled by default)
-	-- incremental_selection = { enable = true },
-	-- refactor = { ... }
-	-- navigation = { ... }
-	-- etc.
+		-- Textobject movement module
+		move = {
+			enable = true,
+			set_jumps = true,
+			goto_next_start = {
+				["]m"] = { query = "@function.outer", desc = "Next function start" },
+				["]]"] = { query = "@class.outer", desc = "Next class start" },
+				["]o"] = { query = "@loop.*", desc = "Next loop start" },
+				["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope start" },
+				["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold start" },
+				["]?"] = { query = "@conditional.outer", desc = "Next conditional start" },
+				["]a"] = { query = "@parameter.inner", desc = "Next argument start" },
+			},
+			goto_next_end = {
+				["]M"] = { query = "@function.outer", desc = "Next function end" },
+				["]["] = { query = "@class.outer", desc = "Next class end" },
+				["]O"] = { query = "@loop.*", desc = "Next loop end" },
+				["]A"] = { query = "@parameter.inner", desc = "Next argument end" },
+			},
+			goto_previous_start = {
+				["[m"] = { query = "@function.outer", desc = "Previous function start" },
+				["[["] = { query = "@class.outer", desc = "Previous class start" },
+				["[o"] = { query = "@loop.*", desc = "Previous loop start" },
+				["[s"] = { query = "@scope", query_group = "locals", desc = "Previous scope start" },
+				["[z"] = { query = "@fold", query_group = "folds", desc = "Previous fold start" },
+				["[?"] = { query = "@conditional.outer", desc = "Previous conditional start" },
+				["[a"] = { query = "@parameter.inner", desc = "Previous argument start" },
+			},
+			goto_previous_end = {
+				["[M"] = { query = "@function.outer", desc = "Previous function end" },
+				["[]"] = { query = "@class.outer", desc = "Previous class end" },
+				["[O"] = { query = "@loop.*", desc = "Previous loop end" },
+				["[A"] = { query = "@parameter.inner", desc = "Previous argument end" },
+			},
+		},
+
+		-- LSP interop module for textobjects
+		lsp_interop = {
+			enable = true,
+			border = "rounded",
+			floating_preview_opts = {},
+			peek_definition_code = {
+				["<leader>df"] = { query = "@function.outer", desc = "Peek function definition" },
+				["<leader>dF"] = { query = "@class.outer", desc = "Peek class definition" },
+			},
+		},
+	},
 })
+
+print("nvim-treesitter configuration loaded.")
