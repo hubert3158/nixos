a05b75cb9b42b8f445b73dd75cc2eb96965c655f
Subash Acharya <hubert3158@gmail.com>
2025-05-08 23:35:05 -0400

    wip

--- nvim/lua/user/conform.lua
+++ nvim/lua/user/conform.lua
@@ -3,13 +3,11 @@ require("conform").setup({
 		lua = { "stylua" },
 		-- Conform will run multiple formatters sequentially
 		python = { "isort", "black" },
-		-- You can customize some of the format options for the filetype (:help conform.format)
 		-- Conform will run the first available formatter
 		javascript = { "prettierd", "prettier", stop_after_first = true },
-
 		typescript = { "prettierd", "prettier", stop_after_first = true },
 		json = { "prettierd", "prettier", stop_after_first = true },
-		sql = { "sqlformat" },
+		sql = { "pg_format" },
 		nix = { "alejandra" },
 		java = { "google-java-format" },
 	},
