bdcff3576f4f18712a40044229b65a423d94ef80
subash <subash@smartmca.com>
2025-05-09 15:36:06 -0400

    wip

--- configuration.nix
+++ configuration.nix
@@ -69,7 +69,7 @@
     bluetooth.enable = true;
   };
 
-  networking.firewall.allowedTCPPorts = [3000 8080 8081 993 5678 5000 8083];
+  networking.firewall.allowedTCPPorts = [3000 8080 8081 993 5678 5432 5000 8083 8085 9990];
 
   # services.intune.enable = true;
   # Set your time zone.
