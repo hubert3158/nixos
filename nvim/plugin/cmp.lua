198a2df70cfd3540daf492d64d35066fe7043bbb
Subash Acharya <hubert3158@gmail.com>
2025-05-13 01:45:47 -0400

    wip

--- nvim/plugin/cmp.lua
+++ nvim/plugin/cmp.lua
@@ -1,14 +1,22 @@
 local cmp = require("blink.cmp")
 local luasnip = require("luasnip")
-
+-- Load friendly snippets
 require("luasnip.loaders.from_vscode").lazy_load()
 luasnip.config.setup({})
 
+-- Snippet keybinding
+vim.keymap.set({ "i", "s" }, "<C-k>", function()
+	if luasnip.expand_or_jumpable() then
+		luasnip.expand_or_jump()
+	end
+end, { silent = true })
+
 cmp.setup({
 	keymap = { preset = "default" },
+	snippets = {
+		preset = "luasnip",
+	},
 	sources = {
-		luasnip = {
-			providers = { "default" },
-		},
+		default = { "lsp", "path", "buffer", "snippets" },
 	},
 })
