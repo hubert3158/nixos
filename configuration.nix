ca86615e131260ff0b1dfe314e178b338c38ca6d
subash <subash@smartmca.com>
2025-05-14 09:18:38 -0400

    wip

--- configuration.nix
+++ configuration.nix
@@ -42,11 +42,6 @@
     "8.8.8.8" # Google
     "8.8.4.4" # Google secondary
   ];
-  fileSystems."/nix/store" = {
-    device = "/dev/disk/by-uuid/9c498fa9-290c-44ef-914f-aa0987369009";
-    fsType = "ext4";
-    options = ["defaults"];
-  };
 
   # Display manager and keymap
   services.xserver = {
--- home.nix
+++ home.nix
@@ -1,9 +1,2 @@
-{
-  ...
-}: {
-  fileSystems."/nix/store" = {
-    device = "/dev/disk/by-uuid/9c498fa9-290c-44ef-914f-aa0987369009";
-    fsType = "ext4";
-    options = ["defaults"];
-  };
+{...}: {
 }
