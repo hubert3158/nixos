da060e7d2a4323491c981cff9e340173dcaa49c8
Subash Acharya <hubert3158@gmail.com>
2025-05-14 03:36:31 -0400

    wip

--- home-manager-config-files/xdg.nix
+++ home-manager-config-files/xdg.nix
@@ -7,8 +7,8 @@
         genericName = "Application";
         exec = "evolution";
         terminal = false;
-        categories = [ "Email" "Application" "Network" ];
-        mimeType = [ "text/html" "text/xml" ];
+        categories = ["Email" "Application" "Network"];
+        mimeType = ["text/html" "text/xml"];
         comment = "It is a email client bro";
       };
       swayimg = {
@@ -16,8 +16,8 @@
         genericName = "Image Viewer";
         exec = "swayimg %f";
         terminal = false;
-        categories = [ "Graphics" "Viewer" ];
-        mimeType = [ "image/jpeg" "image/png" "image/gif" "image/bmp" "image/webp" "image/tiff" ];
+        categories = ["Graphics" "Viewer"];
+        mimeType = ["image/jpeg" "image/png" "image/gif" "image/bmp" "image/webp" "image/tiff"];
         comment = "It opens images bro";
       };
 
@@ -26,8 +26,8 @@
         genericName = "file editor";
         exec = "nvim %f";
         terminal = true;
-        categories = [ "TextEditor" "Utility" ];
-        mimeType = [ "text/plain"];
+        categories = ["TextEditor" "Utility"];
+        mimeType = ["text/plain"];
         comment = "Its a beast bro";
       };
       workspacesclient = {
@@ -35,36 +35,35 @@
         genericName = "AWS Workspaces Client";
         exec = "workspacesclient";
         terminal = false;
-        categories = [ "Network" "Utility" ];
-        mimeType = [ "text/plain" ];
+        categories = ["Network" "Utility"];
+        mimeType = ["text/plain"];
         comment = "needed at work bro";
       };
-          chromeDebugMode = {
-      name = "Chrome Debug Mode";
-      genericName = "Chrome Debug Mode";
-      exec = "google-chrome-stable --remote-debugging-port=9222 --no-first-run --new-instance";
-      terminal = false;
-      categories = [ "Network" ]; 
-      mimeType = [ "text/plain" ];
-      comment = "needed at work bro";
+      chromeDebugMode = {
+        name = "Chrome Debug Mode";
+        genericName = "Chrome Debug Mode";
+        exec = "google-chrome-stable --remote-debugging-port=9222 --no-first-run --new-instance";
+        terminal = false;
+        categories = ["Network"];
+        mimeType = ["text/plain"];
+        comment = "needed at work bro";
+      };
     };
-
-    };
-    mime = { enable = true; };
+    mime = {enable = true;};
     mimeApps = {
       enable = true;
       defaultApplications = {
-        "image/jpeg" = [ "swayimg.desktop" ];
-        "image/png" = [ "swayimg.desktop" ];
-        "image/gif" = [ "swayimg.desktop" ];
-        "image/bmp" = [ "swayimg.desktop" ];
-        "image/webp" = [ "swayimg.desktop" ];
-        "image/tiff" = [ "swayimg.desktop" ];
-        "application/pdf" = [ "microsoft-edge-stable.desktop" ];
-        "x-scheme-handler/msteams"=["teams-for-linux.desktop"];
-        "text/plain"=["nvim.desktop"];
+        "image/jpeg" = ["swayimg.desktop"];
+        "image/png" = ["swayimg.desktop"];
+        "image/gif" = ["swayimg.desktop"];
+        "image/bmp" = ["swayimg.desktop"];
+        "image/webp" = ["swayimg.desktop"];
+        "image/tiff" = ["swayimg.desktop"];
+        "application/pdf" = ["microsoft-edge.desktop"];
+        "x-scheme-handler/msteams" = ["teams-for-linux.desktop"];
+        "text/plain" = ["nvim.desktop"];
+        "text/markdown" = ["google-chrome-stable.desktop"];
       };
     };
   };
 }
-
