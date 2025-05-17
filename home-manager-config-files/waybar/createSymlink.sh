12ae5f5f803e37fce6ed2e3b6d55df96e6bbb3ba
Subash Acharya <hubert3158@gmail.com>
2025-05-17 02:06:10 -0400

    fully removed waybar

--- home-manager-config-files/waybar/createSymlink.sh
+++
@@ -1,7 +0,0 @@
-#!/etc/profiles/per-user/hubert/bin/zsh
- 
-mkdir -p ~/.config/waybar
-
-ln -sf ~/nixos/home-manager-config-files/waybar/config.jsonc ~/.config/waybar/config.jsonc 
-ln -sf ~/nixos/home-manager-config-files/waybar/style.css  ~/.config/waybar/style.css
-
--- home-manager-config-files/waybar/scripts/outlook.sh
+++
@@ -1,3 +0,0 @@
-#!/etc/profiles/per-user/hubert/bin/zsh
-microsoft-edge-stable --app=https://chat.openai.com
-
