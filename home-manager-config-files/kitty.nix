e43e6eebb442346839c84f9f469b9b3d0cabfd79
Subash Acharya <hubert3158@gmail.com>
2025-05-21 00:27:33 -0400

    wip

--- home-manager-config-files/kitty.nix
+++ home-manager-config-files/kitty.nix
@@ -7,13 +7,12 @@
       package = pkgs.nerd-fonts.monofur;
     };
     settings = {
-      shell = "${pkgs.zsh}/bin/zsh --login";
+      shell = "${pkgs.fish}/bin/fish --login";
 
       scrollback_lines = 10000;
       enable_audio_bell = false;
       update_check_interval = 0;
 
-      # Set Dracula background
       background = "#282a36";
       background_image = "~/nixos/images/kitty-wallpaper.jpg";
       background_image_layout = "cscaled";
@@ -24,24 +23,9 @@
       cursor = "#f8f8f0";
       cursor_text_color = "#282a36";
 
-      # Color settings
-      color0 = "#21222c";
-      color1 = "#ff5555";
-      color2 = "#50fa7b";
-      color3 = "#f1fa8c";
-      color4 = "#bd93f9";
-      color5 = "#ff79c6";
-      color6 = "#8be9fd";
-      color7 = "#f8f8f2";
-      color8 = "#6272a4";
-      color9 = "#ff6e6e";
-      color10 = "#69ff94";
-      color11 = "#ffffa5";
-      color12 = "#d6acff";
-      color13 = "#ff92df";
-      color14 = "#a4ffff";
-      color15 = "#ffffff";
+      # Color settings…
     };
+
     keybindings = {
       "ctrl+c" = "copy_or_interrupt";
     };
