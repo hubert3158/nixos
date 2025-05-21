96af3e13676382ad52cd9fc8a90938dd3fcb022d
Subash Acharya <hubert3158@gmail.com>
2025-05-21 00:27:33 -0400

    wip

--- home-manager-config-files/fish.nix
+++ home-manager-config-files/fish.nix
@@ -6,25 +6,13 @@
   programs.fish = {
     enable = true;
     interactiveShellInit = ''
-      set -g fish_greeting ""
-      set -g fish_color_command brcyan
-      set -g fish_color_error brred
-      set -g fish_color_param white
-      set -g fish_color_comment brblack
-      set -g fish_color_operator brgreen
-      set -g fish_color_quote yellow
-      set -g fish_color_end brmagenta
-      set -g fish_color_autosuggestion 555
-      set -g fish_color_search_match --background=purple
+      set -g fish_greeting "brusssssskiii"
       set -Ux EDITOR nvim
       set -Ux VISUAL nvim
       set -Ux PAGER less
-      function fish_prompt
-        set_color $fish_color_cwd
-        echo -n (prompt_pwd)
-        set_color normal
-        echo -n ' ❯ '
-      end
+
+      # Enable Starship prompt
+      starship init fish | source
     '';
   };
 
