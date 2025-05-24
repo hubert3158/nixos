e4291ec5895cd9a706ca8f6354ea914a6c6e9c58
Subash Acharya <hubert3158@gmail.com>
2025-05-24 01:34:31 -0400

    wip

--- home-manager-config-files/ghostty.nix
+++ home-manager-config-files/ghostty.nix
@@ -1,5 +1,28 @@
 {pkgs, ...}: {
   programs.ghostty = {
     enable = true;
+    installVimSyntax = true;
+    installBatSyntax = true;
+    settings = {
+      "copy-on-select" = true;
+      "font-family" = "monofur nerd font";
+      "font-size " = 18;
+      "font-style" = "light";
+      "font-feature" = "-calt,-liga,-dlig";
+      "font-style-bold" = "false";
+      "shell-integration" = "fish";
+      "theme" = "Arthur";
+      "background-opacity" = 0.7;
+    };
   };
 }
+# copy-on-select = true
+# font-family = monofur nerd font
+# font-size = 18
+# font-style = light
+# font-feature = "-calt,-liga,-dlig"
+# font-style-bold = false
+# shell-integration = fish
+# theme = Arthur
+# background-opacity = 0.7
+
