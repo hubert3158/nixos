55415e41ea6cce95bee1c3194a658a89410bb4bd
Subash Acharya <hubert3158@gmail.com>
2025-05-24 02:27:18 -0400

    wip

--- home-manager-config-files/tmux.nix
+++ home-manager-config-files/tmux.nix
@@ -1,44 +1,23 @@
 # neovim.nix
-{ pkgs, ... }:
-{
- programs.tmux = {
-	enable = true;
-	clock24 = true;
-	plugins = with pkgs.tmuxPlugins; [
-		sensible
-		yank
+{pkgs, ...}: {
+  programs.tmux = {
+    enable = true;
+    clock24 = true;
+    plugins = with pkgs.tmuxPlugins; [
+      sensible
+      yank
       resurrect
-		{
-		plugin = catppuccin;
-			extraConfig = ''
-set -g @catppuccin_window_left_separator ""
-set -g @catppuccin_window_right_separator " "
-set -g @catppuccin_window_middle_separator " █"
-set -g @catppuccin_window_number_position "right"
+      {
+        plugin = catppuccin;
+        extraConfig = ''
+        '';
+      }
+    ];
 
-set -g @catppuccin_window_default_fill "number"
-set -g @catppuccin_window_default_text "#W"
+    extraConfig = ''
+      set -g mouse on
+      set-option -g allow-passthrough all
 
-set -g @catppuccin_window_current_fill "number"
-set -g @catppuccin_window_current_text "#W"
-
-set -g @catppuccin_status_modules_right "directory user host session"
-set -g @catppuccin_status_left_separator  " "
-set -g @catppuccin_status_right_separator ""
-set -g @catppuccin_status_fill "icon"
-set -g @catppuccin_status_connect_separator "no"
-
-set -g @catppuccin_directory_text "#{pane_current_path}"
-			'';
-		}
-	];
-
-    	extraConfig = ''
-		set -g mouse on
-		set-option -g allow-passthrough all
-
-	'';
-
-};
+    '';
+  };
 }
-
