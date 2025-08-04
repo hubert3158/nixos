dc6d32e5019d9cc9200ff75396ed0bd06ff7412e
subash <subash@smartmca.com>
2025-08-04 14:33:50 -0400

    sql lint as postgresql

--- nvim/lua/user/nvimLint.lua
+++ nvim/lua/user/nvimLint.lua
@@ -1,3 +1,24 @@
+require("lint").linters.sqlfluff = {
+	name = "sqlfluff",
+	cmd = "sqlfluff",
+	stdin = false,
+	args = { "lint", "--dialect", "postgres", "--format", "json", "--nocolor" },
+	stream = "stdout",
+	ignore_exitcode = true,
+	parser = require("lint.parser").from_errorformat("%f:%l:%c: %t%*[^:]: %m", {
+		source = "sqlfluff",
+		severity = {
+			["L"] = vim.diagnostic.severity.WARN,
+			["R"] = vim.diagnostic.severity.WARN,
+			["C"] = vim.diagnostic.severity.WARN,
+			["D"] = vim.diagnostic.severity.WARN,
+			["S"] = vim.diagnostic.severity.WARN,
+			["E"] = vim.diagnostic.severity.ERROR,
+			["F"] = vim.diagnostic.severity.ERROR,
+		},
+	}),
+}
+
 require("lint").linters_by_ft = {
 	markdown = { "markdownlint" }, -- No specific markdown LSP in your list; markdownlint is standard.
 	yaml = { "yamllint" }, -- No specific YAML LSP in your list; yamllint is standard.
