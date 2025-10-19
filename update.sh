9a263515c7dd8202123775bba7e72b9e86a5fd1f
Subash Acharya <hubert3158@gmail.com>
2025-10-19 17:24:44 -0400

    wip

--- update.sh
+++ update.sh
@@ -1,5 +1,19 @@
-clean-all
+#!/usr/bin/env zsh
+
+clean_all() {
+    sudo nix-env -v --delete-generations +10 --profile ~/.local/state/nix/profiles/home-manager &&
+        sudo nix-env -v --delete-generations +10 --profile /nix/var/nix/profiles/system &&
+        sudo nix-collect-garbage -v -d &&
+        sudo nix-store --optimise
+}
+
+
+clean_all
+sleep 5
 nix flake update
+sleep 5
 sudo nixos-rebuild switch --flake .#home --upgrade --show-trace
+sleep 5
 clean-all
+sleep 5
 reboot
