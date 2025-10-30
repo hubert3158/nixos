57d705ace8232918168b3d2fbb94384e099b38db
Subash Acharya <hubert3158@gmail.com>
2025-10-29 22:32:28 -0400

    wip

--- overlays/default.nix
+++ overlays/default.nix
@@ -7,33 +7,6 @@
     # })
 
     # Overlay to disable checks for packages with failing tests
-    (final: prev: {
-      python3Packages =
-        prev.python3Packages
-        // {
-          psycopg = prev.python3Packages.psycopg.overridePythonAttrs (old: {
-            doCheck = false;
-          });
-          pytest-postgresql = prev.python3Packages.pytest-postgresql.overridePythonAttrs (old: {
-            doCheck = false;
-          });
-          sqlframe = prev.python3Packages.sqlframe.overridePythonAttrs (old: {
-            doCheck = false;
-          });
-          narwhals = prev.python3Packages.narwhals.overridePythonAttrs (old: {
-            doCheck = false;
-          });
-          pyreadstat = prev.python3Packages.pyreadstat.overridePythonAttrs (old: {
-            doCheck = false;
-          });
-          pandas-stubs = prev.python3Packages.pandas-stubs.overridePythonAttrs (old: {
-            doCheck = false;
-          });
-          pdfplumber = prev.python3Packages.pdfplumber.overridePythonAttrs (old: {
-            doCheck = false;
-          });
-        };
-    })
     (import ../nix/neovim-overlay.nix {inherit inputs;})
   ];
 }
