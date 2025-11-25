75b91031a11819b183901e17991ce8baf4b1d2fd
subash <subash@smartmca.com>
2025-11-25 10:22:22 -0500

    wip

--- nvim/init.lua
+++ nvim/init.lua
@@ -958,8 +958,23 @@ vim.keymap.set("n", "<leader>ko", function()
 end, { noremap = true, silent = true, desc = "Open request in new buffer" })
 
 vim.keymap.set("n", "<leader>kt", function()
-	require("kulala").toggle_view()
-end, { noremap = true, silent = true, desc = "Toggle between request/response views" })
+	-- Check if any Kulala windows are open
+	local kulala_open = false
+	for _, win in ipairs(vim.api.nvim_list_wins()) do
+		local buf = vim.api.nvim_win_get_buf(win)
+		local buf_name = vim.api.nvim_buf_get_name(buf)
+		if string.match(buf_name, "kulala://") then
+			kulala_open = true
+			break
+		end
+	end
+
+	if kulala_open then
+		require("kulala").close()
+	else
+		require("kulala.ui").show_body()
+	end
+end, { noremap = true, silent = true, desc = "Toggle Kulala windows on/off" })
 
 vim.keymap.set("n", "<leader>kf", function()
 	require("kulala").search()
