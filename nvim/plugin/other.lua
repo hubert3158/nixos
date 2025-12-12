928bfa44a5a1447d8ca70ea5b8377f4499d010d2
Subash Acharya <hubert3158@gmail.com>
2025-12-12 02:43:10 -0500

    wip

--- nvim/plugin/other.lua
+++ nvim/plugin/other.lua
@@ -69,11 +69,3 @@ require("lualine").setup({
 
 -- Comment
 require("Comment").setup()
-
-vim.cmd([[
-  hi Normal guibg=NONE ctermbg=NONE
-  hi NormalNC guibg=NONE ctermbg=NONE
-  hi EndOfBuffer guibg=NONE ctermbg=NONE
-  hi LineNr guibg=NONE ctermbg=NONE
-  hi SignColumn guibg=NONE ctermbg=NONE
-]])
