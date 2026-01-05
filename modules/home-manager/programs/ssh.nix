cb4f78285e8040b78c3bc49fb0cfd7d5be3e058d
subash <subash@smartmca.com>
2026-01-05 11:08:49 -0500

    wip

--- modules/home-manager/programs/ssh.nix
+++ modules/home-manager/programs/ssh.nix
@@ -1,10 +1,12 @@
 # SSH client configuration
-{ config, lib, pkgs, ... }:
-
-let
-  cfg = config.modules.programs.ssh;
-in
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.modules.programs.ssh;
+in {
   options.modules.programs.ssh = {
     enable = lib.mkEnableOption "SSH client configuration";
 
@@ -38,73 +40,75 @@ in
     programs.ssh = {
       enable = true;
       enableDefaultConfig = false;
-      matchBlocks = {
-        "*" = {
-          forwardAgent = cfg.enableForwardAgent;
-          forwardX11 = false;
-          forwardX11Trusted = true;
-          serverAliveInterval = cfg.serverAliveInterval;
-          serverAliveCountMax = cfg.serverAliveCountMax;
-          extraOptions = {
-            KexAlgorithms = "sntrup761x25519-sha512@openssh.com,curve25519-sha256,curve25519-sha256@libssh.org";
+      matchBlocks =
+        {
+          "*" = {
+            forwardAgent = cfg.enableForwardAgent;
+            forwardX11 = false;
+            forwardX11Trusted = true;
+            serverAliveInterval = cfg.serverAliveInterval;
+            serverAliveCountMax = cfg.serverAliveCountMax;
+            extraOptions = {
+              KexAlgorithms = "sntrup761x25519-sha512@openssh.com,curve25519-sha256,curve25519-sha256@libssh.org";
+            };
           };
-        };
-      } // (lib.optionalAttrs cfg.enableWorkHosts {
-        "stg" = {
-          hostname = "REDACTED-HOST-STG";
-          user = "ubuntu";
-          port = 22;
-          identityFile = "~/.ssh/staging.v2.pem";
-        };
+        }
+        // (lib.optionalAttrs cfg.enableWorkHosts {
+          "stg" = {
+            hostname = "0.0.0.0";
+            user = "ubuntu";
+            port = 22;
+            identityFile = "~/.ssh/staging.v2.pem";
+          };
 
-        "prod" = {
-          hostname = "REDACTED-HOST-PROD";
-          user = "ubuntu";
-          port = 22;
-          identityFile = "~/.ssh/prod.v2.pem";
-        };
+          "prod" = {
+            hostname = "REDACTED-HOST-PROD";
+            user = "ubuntu";
+            port = 22;
+            identityFile = "~/.ssh/prod.v2.pem";
+          };
 
-        "wellMed" = {
-          hostname = "0.0.0.0";
-          user = "ubuntu";
-          port = 22;
-        };
+          "wellMed" = {
+            hostname = "0.0.0.0";
+            user = "ubuntu";
+            port = 22;
+          };
 
-        "submission" = {
-          hostname = "0.0.0.0";
-          user = "ubuntu";
-          port = 22;
-          identityFile = "~/.ssh/automated_submission.pem";
-        };
+          "submission" = {
+            hostname = "0.0.0.0";
+            user = "ubuntu";
+            port = 22;
+            identityFile = "~/.ssh/automated_submission.pem";
+          };
 
-        "dev" = {
-          hostname = "0.0.0.0";
-          user = "ubuntu";
-          port = 22;
-          identityFile = "~/.ssh/dev_server.pem";
-        };
+          "dev" = {
+            hostname = "0.0.0.0";
+            user = "ubuntu";
+            port = 22;
+            identityFile = "~/.ssh/dev_server.pem";
+          };
 
-        "demo" = {
-          hostname = "0.0.0.0";
-          user = "ubuntu";
-          port = 22;
-          identityFile = "~/.ssh/demo.pem";
-        };
+          "demo" = {
+            hostname = "0.0.0.0";
+            user = "ubuntu";
+            port = 22;
+            identityFile = "~/.ssh/demo.pem";
+          };
 
-        "REDACTED-BT" = {
-          hostname = "REDACTED-HOST-BT";
-          user = "ubuntu";
-          port = 22;
-          identityFile = "~/.ssh/REDACTED-BT.pem";
-        };
+          "REDACTED-BT" = {
+            hostname = "REDACTED-HOST-BT";
+            user = "ubuntu";
+            port = 22;
+            identityFile = "~/.ssh/REDACTED-BT.pem";
+          };
 
-        "rules-engine" = {
-          hostname = "0.0.0.0";
-          user = "ubuntu";
-          port = 22;
-          identityFile = "~/.ssh/rules_engine.pem";
-        };
-      });
+          "rules-engine" = {
+            hostname = "0.0.0.0";
+            user = "ubuntu";
+            port = 22;
+            identityFile = "~/.ssh/rules_engine.pem";
+          };
+        });
     };
   };
 }
