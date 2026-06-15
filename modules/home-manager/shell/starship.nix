caf5873d169841620d21b9570c1ed4edf179a4fd
subash <subash@smartmca.com>
2026-06-15 12:07:04 -0400

    wip

--- modules/home-manager/shell/starship.nix
+++ modules/home-manager/shell/starship.nix
@@ -13,7 +13,7 @@ in
     programs.starship = {
       enable = true;
       settings = {
-        scan_timeout = 30;
+        scan_timeout = 1000;
         add_newline = true;
         character = {
           success_symbol = "[➜](bold green)";
